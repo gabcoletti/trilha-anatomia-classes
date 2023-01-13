f0893998f7e1ea33343a65d691e9b59a93b34a31
gabcoletti <gabecoletti@gmail.com>
2023-01-13 11:00:55 -0300

    Anatomia de Classes - 5

---
+++ src/edu/gabriela/primeirasemana/BoletimEstudandil.java
@@ -0,0 +1,16 @@
+package edu.gabriela.primeirasemana;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class BoletimEstudandil {
+
+    public static void main (String[] args) {
+        int mediaFinal = 7;
+        if(mediaFinal<6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal==6)
+            System.out.println("PROVA MINERVA");
+        else 
+            System.out.println("APROVADO");
+    }
+}
