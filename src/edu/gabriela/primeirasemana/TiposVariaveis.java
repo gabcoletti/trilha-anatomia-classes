0e6b4fd2b51d04b4c3c49a3146c9d45b19f9841c
gabcoletti <gabecoletti@gmail.com>
2023-01-13 14:04:09 -0300

    Tipos e Variaveis  1 - 2 - 3 - 4

---
+++ src/edu/gabriela/primeirasemana/TiposVariaveis.java
@@ -0,0 +1,26 @@
+package edu.gabriela.primeirasemana;
+
+public class TiposVariaveis {
+    public static void main(String [] args) throws Exception {
+        //tipos primitivos
+        //estudem a classe String que representa o texto na aplicação
+
+        String meuNome = "GLEYSON SAMPAIO";
+
+        double salarioMinimo = 2500;
+        
+        short numeroCurto = 1 ;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+        
+        final double VALOR DE PI = 3.14
+
+    }
+    
+}
