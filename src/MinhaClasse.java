dc72c639db0f8563ac830d00a71742879fce880f
gabcoletti <gabecoletti@gmail.com>
2023-01-13 10:24:47 -0300

    Anatomia de Classes -3

---
+++ src/MinhaClasse.java
@@ -0,0 +1,16 @@
+public class MinhaClasse  {
+
+    public static void main (String [] args) {
+
+        String primeiroNome = "Gabriela";
+        String segundoNome = "Coletti";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+        }
+
+    public static String nomeCompleto (String primerioNome, String segundoNome) {
+        return "Resultado do método" + primeiroNome.concat (" ").concat(segundoNome);
+    }
+}
